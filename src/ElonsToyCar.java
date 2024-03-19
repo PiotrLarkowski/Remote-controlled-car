a89f2e7e6e0b107c587343e1aa947539a2fd3f37
Piotr Larkowski <p.larkowski90@gmail.com>
2024-03-19 21:37:11 +0100

    Implement method to add 20 meters when use method drive and add two private variable to ElonsToyCar class

--- src/ElonsToyCar.java
+++ src/ElonsToyCar.java
@@ -1,11 +1,21 @@
 public class ElonsToyCar {
+    private String distance = "0";
+    private String batteryCharge = "100%";
     public static ElonsToyCar buy() {
-        return new ElonsToyCar();
+        ElonsToyCar car = new ElonsToyCar();
+        return car;
     }
+
     public String distanceDisplay() {
-        return "Driven 0 meters";
+        return "Driven "+distance+" meters";
     }
+
     public String batteryDisplay() {
         return "Battery at 100%";
+    }
+
+    public void drive() {
+        int passed = Integer.parseInt(distance) + 20;
+        distance = ""+passed;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,10 @@ public class Main {
         System.out.println(car.distanceDisplay());
         System.out.println("Show me how much battery is left");
         System.out.println(car.batteryDisplay());
+        car.drive();
+        car.drive();
+        System.out.println("Show me how many meters the car traveled");
+        System.out.println(car.distanceDisplay());
     }
 
 }
